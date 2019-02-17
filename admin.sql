4f118e4f95f4dea93fface296acd71952174ddd3
Michael <michaeljefferies08@gmail.com>
2019-02-17 22:53:25 +0000

    Updated login database

--- admin.sql
+++ admin.sql
@@ -2,10 +2,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `first_name` text NOT NULL,
   `last_name` text NOT NULL,
+  `email` VARCHAR(320) NOT NULL,
   `mob_no` int(11) NOT NULL,
-  `user_name` varchar(20) NOT NULL,
-  `password` varchar(15) NOT NULL,
+  `user_name` varchar(40) NOT NULL,
+  `password` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
-INSERT INTO users(first_name,last_name,mob_no,user_name,password) VALUES('admin','admin','99999999999','admin','admin');
+INSERT INTO users(first_name,last_name,email,mob_no,user_name,password) VALUES ('Jichael','Mefferies','admin@netsoc.co',1234567890,'my_own_username','admin');

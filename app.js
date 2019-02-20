945891f19343f0568ec4e9f80bf348a77120e5a2
Michael <michaeljefferies08@gmail.com>
2019-02-20 13:53:17 +0000

    Revert "add apply.ejs"
    This reverts commit e930428b3a3dbb24c6e35cfc4df66aa28a16763c.

--- app.js
+++ app.js
@@ -19,22 +19,12 @@ var connection = mysql.createConnection({
     password : 'JCDQcTNBknX',
     database : 'mj_a_klaas_database',
     multipleStatements: true
-});
+  });
 
-// var connection = mysql.createConnection({
-//   host     : '127.0.0.1',
-//   user     : 'root',
-//   password : '12345678',
-//   database : 'nodemysql'
-//   // multipleStatements: true
-// });
-
-
-// Connect
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to mysql database...");
-  // var sql = "INSERT INTO users(first_name,last_name,mob_no,user_name,password) VALUES ('123','123','123','123','123')";
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to mysql database!");
+    // var sql = "INSERT INTO users(first_name,last_name,mob_no,user_name,password) VALUES ('123','123','123','123','123')";
 });
 
 global.db = connection; // multiple things need this object.
@@ -75,94 +65,4 @@ app.get('/logout',user.logout);
 
 app.get("/submission",upload.uploadFile);
 app.post("/submission",upload.uploadFile);
-
-app.get("/apply",user.apply);
-
-// // Create DB
-// app.get('/createdb', (req, res) => {
-//   let sql = 'CREATE DATABASE nodemysql'
-//   connection.query(sql, (err, result) => {
-//     if(err) throw err;
-//     console.log(result);
-//     res.send('database created...');
-//   });
-// });
-
-// // Create table
-// app.get('/createpoststable', (req, res) => {
-//   let sql = 'create table posts(id int auto_increment, title varchar(255), body varchar(255), primary key(id))';
-//   connection.query(sql, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Posts table created...')
-//   });
-// });
-
-// // Insert post 1
-// app.get('/addpost1', (req, res) => {
-//   let post = {title:'Post One', body:'This is post number one'};
-//   let sql = 'insert into posts set ?';
-//     let query = connection.query(sql, post, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Posts 1 added...');
-//   });
-// });
-
-// // Insert post 2
-// app.get('/addpost2', (req, res) => {
-//   let post = {title:'Post Two', body:'This is post number two'};
-//   let sql = 'insert into posts set ?';
-//     let query = connection.query(sql, post, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Posts 2 added...');
-//   });
-// });
-
-// // Select post
-// app.get('/getposts', (req, res) => {
-//   let sql = 'select * from posts';
-//     let query = connection.query(sql, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Posts fetched...');
-//   });
-// });
-
-// // Select single post
-// app.get('/getpost/:id', (req, res) => {
-//   let sql = `select * from posts where id = ${req.params.id}`;
-//     let query = connection.query(sql, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Post fetched...');
-//   });
-// });
-
-
-// // Update title
-// app.get('/updatepost/:id', (req, res) => {
-//   let newTitle = 'Updated Title';
-//   let sql = `update posts set title = '${newTitle}' where id = ${req.params.id}`;
-//     let query = connection.query(sql, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Post updated...');
-//   });
-// });
-
-
-// // Delete post
-// app.get('/deletepost/:id', (req, res) => {
-//   let sql = `delete from posts where id = ${req.params.id}`;
-//     let query = connection.query(sql, (err, result) => {
-//       if(err) throw err;
-//       console.log(result);
-//       res.send('Post deleted...');
-//   });
-// });
-
-app.listen('3001', () => {
-  console.log("Server started on port 3001");
-});
+app.listen('3001');

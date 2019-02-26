ad9bfca3e18f4f4f00ebd3ac5dd043b3ed976ec4
Lance <118100065@umail.ucc.ie>
2019-02-26 22:06:40 +0000

    amend files

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var express = require('express'),
     http = require('http'),
     user = require('./routes/user'),
     upload = require('./routes/upload'),
-<<<<<<< HEAD
+
     path = require('path'),
     apply = require('./routes/apply');
-=======
+
     passwordReset = require('./routes/passwordReset'),
     reviewApplication = require('./routes/reviewApplication'),
     funding = require('./routes/funding'),
     path = require('path');
->>>>>>> e2fed4d32353acbaf4d8fa3fba24c3dece35c299
+
 
 var session = require('express-session') // cookie handler
 var app = express(); // initialise express object.
@@ -100,12 +100,12 @@ app.get('/logout',user.logout);
 app.get("/submission",upload.uploadFile);
 app.post("/submission",upload.uploadFile);
 
-<<<<<<< HEAD
+
 app.get("/apply", apply.storeApplications);
 app.post("/apply", apply.storeApplications);
 
-app.listen('3001');
-=======
+
+
 
 app.get("/apply",user.apply);
 app.post("/apply",user.apply);
@@ -156,4 +156,4 @@ app.delete("/displayAllProposal/:id",user.displayAllProposal_delete);
 app.listen('3001', () => {
   console.log("Server started on port 3001");
 });
->>>>>>> e2fed4d32353acbaf4d8fa3fba24c3dece35c299
+

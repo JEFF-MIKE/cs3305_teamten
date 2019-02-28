6d751ef3889f7bcbfdb3c276adcc04ea4a7ee1d2
Tim Lehane <116300763@umail.ucc.ie>
2019-02-28 01:04:47 +0000

    Made changes to funding to make it work properly

--- routes/funding.js
+++ routes/funding.js
@@ -1,15 +1,12 @@
 // home page
-exports.index = (req, res) => {
+exports.funding = (req, res) => {
   function fetchCalls(data,callback){
       // function for getting SQL data of active calls for proposals
       let sql = "SELECT * FROM calls where active_status = 1"
       db.query(sql, (err,rows) => {
-          if (err){
-              callback(err,null);
-          } else {
-              console.log(rows[0]);
-              console.log("This is id of call from table:" + rows[0].call_id);
-              callback(null,rows[0]);
+          if (err) throw err;
+          if (rows.length) {
+            return res.render('passwordReset.ejs',{rows:rows});
           }
       });
   }

de4168c28888a73e86281e5bb04c7a2a90b87830
davidfox11 <32780427+davidfox11@users.noreply.github.com>
2019-03-02 18:53:27 +0000

    Updated DB table

--- sql/application_table_creation.sql
+++ sql/application_table_creation.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS applications;
 CREATE TABLE applications(
 applicant_id VARCHAR(10) NOT NULL,
 application_id INT AUTO_INCREMENT,
-time_of_submission VARCHAR(20),
+time_of_submission VARCHAR(50),
 amount_requested VARCHAR(10),
 file_name VARCHAR(30),
 cover_note VARCHAR(1000),

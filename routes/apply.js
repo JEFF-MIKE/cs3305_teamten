4976a333c9d3cd96bdf4e7747d147b3afa7c3132
davidfox11 <32780427+davidfox11@users.noreply.github.com>
2019-03-03 21:51:14 +0000

    Revert "Updating apply.js"
    This reverts commit 3b68f8617861b178fe7c108f2bd39f54f4183c28.

--- routes/apply.js
+++ routes/apply.js
@@ -17,7 +17,9 @@ exports.storeApplications = (req,res) => {
     if (req.method === "POST"){
         console.log("POST PART EXECUTED");
         // this is when you submit data
+        let name = req.body.name;
         let applicant_id = req.body.fundingID;
+        let call_id = 12;
         let time_of_submission = new Date();
         let submission_version = 1;
         let amount_requested = req.body.amount_requested;
@@ -37,14 +39,36 @@ exports.storeApplications = (req,res) => {
         res.end()
     })
     */
-        var sql = "INSERT INTO `applications` (applicant_id, time_of_submission,amount_requested, cover_note, submission_version,group_id) VALUES (?,?,?,?,?,?);";
-        db.query(sql,[applicant_id,time_of_submission,amount_requested,cover_note,submission_version,group_name] ,function(err, result) {
-        if (err) throw err;
+    var sql = "INSERT INTO `applications` (applicant_id, call_id, time_of_submission,amount_requested, cover_note, submission_version,group_id) VALUES (?,?,?,?,?,?,?);";
+    db.query(sql,[applicant_id, call_id, time_of_submission,amount_requested,cover_note,submission_version,group_name] ,function(err, result) {
+    if (err) throw err;
         console.log("Successfully entered in data");
-        return res.render("apply1.ejs",{status:"Application Successful", success:"Success",userName:user_name});
+
+        var sql1 = "SELECT * FROM users WHERE id = (SELECT funder_user_id FROM calls WHERE call_id = (SELECT call_id FROM applications WHERE application_id = 3))";
+        db.query(sql1, function(err, result, fields){
+        if (err) throw err;
+            console.log(result);
+            var email = result[0].email;
+            var first_name = result[0].first_name;
+            console.log(email);
+            console.log(first_name);
+
+            var message = 'Hello, '+ first_name +'.' + '\n' + 'A researcher named '+ name +' has just applied for a proposal that you are reviewing.' + '\n\n' + 'You can view the application at the link below.' + '\n' + 'https://team10.netsoc.co/application' + '\n' + 'Kind Regards,' + '\n' + 'Science Foundation Ireland';
+            var subject = "New Application from SFI";
+
+            sendEmail(email, subject, message);
+        });
+
+        status = "Thank you for submitting your application. It has been sent to the reviewers and they will be in touch with you soon."
+        return res.render("apply1.ejs",{status:status, success:"",userName:user_name});
         });
     } else {
         // this is a get 
         return res.render('apply1.ejs',{status:status,success:success,userName:user_name});
     }
+
+    //Provide functionality to email the funder that this proposal is related to
+    
+
 }
+    

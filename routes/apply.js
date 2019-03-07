a7e1408a6861b3451dc74edd05918b6320011a18
davidfox11 <32780427+davidfox11@users.noreply.github.com>
2019-03-07 16:59:27 +0000

    Fixed apply.js for email notifications

--- routes/apply.js
+++ routes/apply.js
@@ -1,10 +1,12 @@
 exports.storeApplications = (req,res) => {
-    let status = "";
-    let success = ""
-    let user_name =  '';
-    if (req.session.user_name !== undefined){
-        user_name = req.session.user_name;
+    if (req.session.user_id === undefined && req.session.user_name === undefined) {
+        return res.send("You must be logged in first to continue");
     }
+
+    var sendEmail = require('./sendEmail');
+
+    let status = "";
+    let success = "";
     console.log("FUNCTION EXECUTED")
 
     /*
@@ -18,8 +20,9 @@ exports.storeApplications = (req,res) => {
         console.log("POST PART EXECUTED");
         // this is when you submit data
         let name = req.body.name;
-        let applicant_id = req.body.fundingID;
-        let call_id = 12;
+        let applicant_id = req.session.user_id;
+        let call_id = req.body.fundingID; // grab from body.
+        console.log(call_id);
         let time_of_submission = new Date();
         let submission_version = 1;
         let amount_requested = req.body.amount_requested;
@@ -39,12 +42,24 @@ exports.storeApplications = (req,res) => {
         res.end()
     })
     */
+
+   var app_id;
+   var sql3 = 'SELECT application_id FROM applications WHERE application_id = (SELECT MAX(application_id) FROM applications)';
+   db.query(sql3, function(err, result, fields){
+       if (err) throw err;
+           console.log(result);
+           console.log(result[0]);
+           console.log(result[0].application_id);
+           app_id = result[0].application_id + 1;
+           //console.log(application_id);
+   });
+
     var sql = "INSERT INTO `applications` (applicant_id, call_id, time_of_submission,amount_requested, cover_note, submission_version,group_id) VALUES (?,?,?,?,?,?,?);";
     db.query(sql,[applicant_id, call_id, time_of_submission,amount_requested,cover_note,submission_version,group_name] ,function(err, result) {
     if (err) throw err;
         console.log("Successfully entered in data");
 
-        var sql1 = "SELECT * FROM users WHERE id = (SELECT funder_user_id FROM calls WHERE call_id = (SELECT call_id FROM applications WHERE application_id = 3))";
+        var sql1 = 'SELECT * FROM users WHERE id = (SELECT funder_user_id FROM calls WHERE call_id = (SELECT call_id FROM applications WHERE application_id = "'+ app_id +'"))';
         db.query(sql1, function(err, result, fields){
         if (err) throw err;
             console.log(result);
@@ -60,15 +75,25 @@ exports.storeApplications = (req,res) => {
         });
 
         status = "Thank you for submitting your application. It has been sent to the reviewers and they will be in touch with you soon."
-        return res.render("apply1.ejs",{status:status, success:"",userName:user_name});
+        //return res.send("Thank you for submitting your application. It has been sent to the reviewers and they will be in touch with you soon.");
+        return res.render('apply1.ejs',{status:status,success:success});
         });
     } else {
-        // this is a get 
-        return res.render('apply1.ejs',{status:status,success:success,userName:user_name});
+        if (req.query === {}){
+            // return an error message since no parameter was passed through.
+                return res.send("Need a parameter.");
+            }   
+        // this is a get
+        let sql = "SELECT first_name,last_name FROM users WHERE id = ?;";
+        db.query(sql,req.session.user_id,(err,results) => {
+            if (err){
+                console.log(err);
+                return res.send("An error occured!");
+            }
+            let fullName = results[0].first_name + " " + results[0].last_name;
+            let call_id = req.query.call_id;
+            return res.render('apply1.ejs',{status:status,success:success,userName:req.session.user_name,fullName:fullName,call_id:call_id});
+        });
     }
-
-    //Provide functionality to email the funder that this proposal is related to
-    
-
 }
     
